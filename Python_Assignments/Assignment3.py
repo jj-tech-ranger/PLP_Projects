67a09aae4ef45d3ac689ab556ba35ef3042c9cd1
dev-jay-jay <jemutaijemimah@gmail.com>
2025-03-11 22:59:29 +0300

    Updated

--- Python_Assignments/Assignment3.py
+++ Python_Assignments/Assignment3.py
@@ -12,6 +12,6 @@ discount_percentage = float(input("Enter the discount percentage: "))
 final_price = calculate_discount(original_price, discount_percentage)
 
 if final_price < original_price:
-    print(f"The final price after applying the discount is: Ksh.{final_price:.2f}")
+    print(f"The final price after applying the discount is: Ksh {final_price:.2f}")
 else:
-    print(f"No discount applied. The original price is: Ksh.{final_price:.2f}")
+    print(f"No discount applied. The original price is: Ksh {final_price:.2f}")
